8a6ce7d3a6850ffe12fcad8ba80cac6a6a5505ee
sista4788 <63381022+sista4788@users.noreply.github.com>
2020-10-27 18:12:17 -0400

    Converts to hexadecimal

---
+++ Code Wars/RGBToHexConversion.js
@@ -0,0 +1,9 @@
+function rgb(r, g, b){
+    return toHex(r)+toHex(g)+toHex(b);
+  }
+  
+  function toHex(d) {
+      if(d < 0 ) {return "00";}
+      if(d > 255 ) {return "FF";}
+      return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()  //I like this padding part.
+  }
